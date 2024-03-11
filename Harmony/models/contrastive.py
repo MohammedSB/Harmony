another big fix. gradient issue

Harmony/models/contrastive.py
@@ -19,7 +19,7 @@ class ContrastivePath(nn.Module):
         # define the text encoder and peripherals
         text_embed_dim = 512
         self.text_backbone = TextEncoder(embed_dim=text_embed_dim)
-        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07)).cuda()
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
         # check if to whether to use labels 
         if self.use_soft_labels:

Harmony/models/text_encoder.py
@@ -18,12 +18,12 @@ class TextEncoder(torch.nn.Module):
             layers=self.transformer_layers,
             heads=self.transformer_heads,
             attn_mask=self.build_attention_mask(),
-        ).cuda()
+        )
 
-        self.text_embedding = torch.nn.Embedding(self.vocab_size, self.transformer_width).cuda()
-        self.text_positional_embedding = torch.nn.Parameter(torch.empty(self.context_length, self.transformer_width)).cuda()
-        self.text_norm = LayerNorm(self.transformer_width).cuda()
-        self.text_projection = torch.nn.Parameter(torch.empty(self.transformer_width, self.embed_dim)).cuda()
+        self.text_embedding = torch.nn.Embedding(self.vocab_size, self.transformer_width)
+        self.text_positional_embedding = torch.nn.Parameter(torch.empty(self.context_length, self.transformer_width))
+        self.text_norm = LayerNorm(self.transformer_width)
+        self.text_projection = torch.nn.Parameter(torch.empty(self.transformer_width, self.embed_dim), requires_grad=True)
 
         self.init()
 

Harmony/models/vision_transformer.py
@@ -177,7 +177,7 @@ class VisionTransformer(nn.Module):
         self.apply(self._init_weights)
 
         # contrastive projection for clip
-        self.contrastive_projection = nn.Parameter(torch.empty(embed_dim, contrastive_text_embed_dim)).cuda()
+        self.contrastive_projection = nn.Parameter(torch.empty(embed_dim, contrastive_text_embed_dim))
 
         # masked image modeling
         self.masked_im_modeling = masked_im_modeling